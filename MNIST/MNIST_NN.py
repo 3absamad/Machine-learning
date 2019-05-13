98591f2af9ff4b29aff35816c233c7b8735dc98a
Ahmed Abdel Samad <3absamaad@gmail.com>
2019-05-13 12:01:29 +0200

    Update MNIST_NN.py

--- MNIST/MNIST_NN.py
+++ MNIST/MNIST_NN.py
@@ -1,51 +1,55 @@
 from __future__ import absolute_import, division, print_function
 
-# TensorFlow and tf.keras
+# Import Tensorflow and helper libraries
 import tensorflow as tf
 from tensorflow import keras
-
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-# LOAD THE DATA
+# Load the data-set from the keras library
 images = keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = images.load_data()
 
+# Check the Training/Test set size
+print('Training set size:', train_labels.shape[0])
+print('Test set size:', test_labels.shape[0])
 
-# EXPLORE THE DATA
+# Plot a random image from the training data-set, also view the image before scaling.
 plt.figure()
-plt.imshow(train_images[1])
+plt.imshow(train_images[np.random.randint(1, 59999)])
 plt.show()
 
-# SCALE THE DATA
+# Scale the images of the entire data-set
 train_images = train_images/255
 test_images = test_images/255
 
-# PLOT AGAIN
+# Plot random images along with respective labels from the data-set
 plt.figure(figsize=(10, 10))
 for i in range(25):
     plt.subplot(5, 5, i+1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(train_labels[i])
+    j = np.random.randint(1, 59999)
+    plt.imshow(train_images[j], cmap=plt.cm.binary)
+    plt.xlabel(train_labels[j])
 plt.show()
 
-# BUILDING THE MODEL
+# Build the NN model:
+# Model consists of input layer with 28*28 neurons (784 inputs), then another layer of 128 neurons with ReLU activation,
+# and at last an output layer with softmax units for detecting probabilities of the input from 1-10
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-# Compile the model
+# Compile the model, and use ADAM optimization algorithm, and evaluation metric set to accuracy
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Train the model
+# Train the model, using batch size of 64 through 10 epochs
 model.fit(train_images, train_labels, batch_size=64, epochs=10)
 
 # Evaluate accuracy
@@ -54,5 +58,5 @@ test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test Set Accuracy:', test_acc)
 
 predictions = model.predict(test_images)
-print('Prediction: ', np.argmax(predictions[0]))
-print('Actual: ', test_labels[0])
+print('Prediction: ', np.argmax(predictions[10]))
+print('Actual: ', test_labels[10])
