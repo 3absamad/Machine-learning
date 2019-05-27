7703475349aaaa03e32145d034d292cec2db980f
Ahmed Abdel Samad <3absamaad@gmail.com>
2019-05-27 13:54:52 +0200

    Update in line 26

--- MNIST/MNIST_CNN.py
+++ MNIST/MNIST_CNN.py
@@ -23,7 +23,7 @@ EPOCHS = 5
 INPUT_SHAPE = (28, 28, 1)
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=INPUT_SHAPE))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
