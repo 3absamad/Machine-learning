5b401ea9f2c1b665584965ba01db511dffd06f49
Ahmed Abdel Samad <3absamaad@gmail.com>
2019-05-27 13:53:07 +0200

    MNIST using CNNs, and trained model

---
+++ MNIST/MNIST_CNN.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and keras
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import numpy as np
+
+# LOAD THE DATA
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
+print('Training set size:', train_labels.shape[0])
+print('Image Size', train_images.shape)
+print('Test set size:', test_labels.shape[0])
+
+# SCALE THE DATA
+train_images = train_images/255
+test_images = test_images/255
+
+# BUILDING THE MODEL
+BATCH_SIZE = 64
+EPOCHS = 5
+INPUT_SHAPE = (28, 28, 1)
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS)
+
+# Evaluate accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test Set Accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+print('Prediction: ', np.argmax(predictions[9]))
+print('Actual: ', test_labels[9])
+
+# Save the model
+model.save('MNIST_CNN.h5')
+
